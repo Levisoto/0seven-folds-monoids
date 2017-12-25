0965ff853a0f147e5934759c60ac23c820c779f2
Levisoto <levisoto.337@gmail.com>
2017-12-25 11:35:56 -0500

    I finished the problem 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,30 @@
 module Lib where
 
+import Data.Monoid
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+data JoinListBasic a = Empty2
+                     | Single2 a
+                     | Append2 (JoinListBasic a) (JoinListBasic a)
+
+jlbToList :: JoinListBasic a -> [a]
+jlbToList Empty2        = []
+jlbToList (Single2 a)   = [a]
+jlbToList (Append2 l r) = jlbToList l ++ jlbToList r
+
+------------------------------------------------------------------
+-----------------Problem 1----------------------------------------
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) l r = Append (foldMap tag [l,r] ) l r
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
